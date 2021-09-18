a846a6f181ce057b98919a9d2cc59ae7c82a49ec
Farhan Hasan <hasanfarhan33@gmail.com>
2021-09-18 14:38:51 +0300

    Played with 2 node packages

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-const math = require("./math");
-console.log(math);
+// const math = require("./math");
+// console.log(math);
+
+// SHELTER CONTAINS INDEX JS, REQUIRES WHATEVER THAT INDEX FILE EXPORTS
+const cats = require("./shelter");
+console.log("Required an entire folder", cats);
---
+++ shelter/index.js
@@ -0,0 +1,8 @@
+const blue = require("./blue");
+const maria = require("./maria");
+const sadie = require("./sadie");
+
+const allCats = [blue, maria, sadie];
+console.log(allCats);
+
+module.exports = allCats;
