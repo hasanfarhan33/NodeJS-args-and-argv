de8fd397066fcbb5ca68949d7608dfda98dfc664
Farhan Hasan <hasanfarhan33@gmail.com>
2021-09-19 23:01:43 +0300

    Learnt about a few more npm packages

---
+++ Artster/index.js
@@ -0,0 +1,11 @@
+const figlet = require("figlet");
+const colors = require("colors");
+
+figlet("That's pretty neat!", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data.green);
+});
